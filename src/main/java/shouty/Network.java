51821a36de7192adf10e16d8a09e5d57ae4e3a33
HyuEstafet <hyulya.gyuler@estafet.com>
2022-06-01 12:56:15 +0300

    Commit: Cleaning up chpater (second commit, tests running now)

--- src/main/java/shouty/Network.java
+++ src/main/java/shouty/Network.java
@@ -1,4 +1,4 @@
-package shoutyMain;
+package shouty;
 import java.util.ArrayList;
 import java.util.List;
 
--- src/test/java/shouty/HearShoutSteps.java
+++ src/test/java/shouty/HearShoutSteps.java
@@ -1,34 +1,35 @@
 package shouty;
 
+
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.Before;
-import shoutyMain.Network;
-import shoutyMain.Person;
 
 import java.util.HashMap;
 
 import static java.util.Arrays.asList;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class HearShoutSteps {
-   private Person lucy;
-   private Person sean;
-   private String messageFromSean;
-   private Network network;
-   private HashMap <String, Person> people;
+
+    private Person sean;
+    private Person lucy;
+    private String messageFromSean;
+    private Network network;
+    private HashMap<String, Person> people;
 
     @Before
     public void createNetwork() {
-        network = new Network(); // Creating a new network before each scenario runs
-        people = new HashMap<>(); // So we can store a key
+        network = new Network();
+        people = new HashMap<>();
     }
 
     @Given("a person named {word}")
     public void a_person_named(String name) {
-        people.put(name,new Person(network));
+        people.put(name, new Person(network));
     }
+
     @When("Sean shouts {string}")
     public void sean_shouts(String message) throws Throwable {
         people.get("Sean").shout(message);
@@ -36,7 +37,7 @@ public class HearShoutSteps {
     }
 
     @Then("Lucy should hear Sean's message")
-    public void lucy_hears_sean_s_message() throws Throwable {
-        assertEquals(asList(messageFromSean),people.get("Lucy").getMessagesHeard());
+    public void lucy_hears_Sean_s_message() throws Throwable {
+        assertEquals(asList(messageFromSean), people.get("Lucy").getMessagesHeard());
     }
 }
